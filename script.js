198c9502ff9019ab5b64284b7d4c9404af0d5540
Egle-Farias <suporto.eglefarias@gmail.com>
2023-07-07 08:03:59 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+    const button1 = document.querySelector("#click-me-btn");
+    const button2 = document.querySelector("#esconder-casa-btn");
+    const casaImg = document.querySelector("#casa-img");
+
+    button1.addEventListener("click", () => {
+        alert("Boa noite!")
+    });
+
+    button2.addEventListener("click", () => {
+        casaImg.remove();
+    });
+})
