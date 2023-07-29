1ed79ee478b9ac288aa67c800adc79cea7e2cd52
Egle-Farias <suporto.eglefarias@gmail.com>
2023-07-29 20:40:01 -0300

    Inclusão da pagina de financiamento, (input e checkbox)

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-window.addEventListener("load", () => {
-    const button1 = document.querySelector("#click-me-btn");
-    const button2 = document.querySelector("#esconder-casa-btn");
-    const casaImg = document.querySelector("#casa-img");
 
-    button1.addEventListener("click", () => {
-        alert("Boa noite!")
-    });
 
-    button2.addEventListener("click", () => {
-        casaImg.remove();
-    });
-})
 
-function escreverNome()  {
-    escreverNome.innerHtml = "Egle Farias";
-}
 
-window.onload = escreverNome;
+const comCarencia = document.querySelector('#comCarencia');
+const listaSuspensa = document.querySelector('#listaSuspensa');
+
+comCarencia.addEventListener('change',function() {
+    if (this.checked) {
+       listaSuspensa.removeAttribute('hidden'); 
+    }   else {
+        listaSuspensa.setAttribute('hidden','hidden'); 
+    }
+});
