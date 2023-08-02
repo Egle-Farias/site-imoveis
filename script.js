1e8b6de34b185643be6c9ad5395c1db274c31449
Egle-Farias <suporto.eglefarias@gmail.com>
2023-08-02 18:52:22 -0300

    Inclusão da calculadora com o javascript

--- script.js
+++ script.js
@@ -12,3 +12,34 @@ comCarencia.addEventListener('change',function() {
         listaSuspensa.setAttribute('hidden','hidden'); 
     }
 });
+
+const dispoeEntrada = document.querySelector('#dispoeEntrada');
+const listaSuspensa2= document.querySelector('#listaSuspensa2');
+
+dispoeEntrada.addEventListener('change',function() {
+    if (this.checked) {
+        listaSuspensa2.removeAttribute('hidden'); 
+    }   else {
+        listaSuspensa2.setAttribute('hidden','hidden'); 
+    }
+});
+
+    var n1 = document.querySelector('#n1')
+    var n2 = document.querySelector('#n2')
+    var resultado = document.querySelector('span')
+
+    function somar() {
+        resultado.innerHTML = parseInt(n1.value) + parseInt(n2.value)
+    }
+
+    function subtrair() {
+        resultado.innerHTML = parseInt(n1.value) + parseInt(n2.value)
+    }
+
+    function multiplicar() {
+        resultado.innerHTML = parseInt(n1.value) + parseInt(n2.value)
+    }
+
+    function dividir() {
+        resultado.innerHTML = parseInt(n1.value) + parseInt(n2.value)
+    }
